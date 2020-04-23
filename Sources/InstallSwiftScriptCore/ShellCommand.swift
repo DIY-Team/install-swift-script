d290d3f4fd9d10b5abbec6806d95b57dde2b0227
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2020-04-24 04:29:56 +0530

    update implementation with the new dependency

--- Sources/InstallSwiftScriptCore/ShellCommand.swift
+++ Sources/InstallSwiftScriptCore/ShellCommand.swift
@@ -6,50 +6,40 @@
 //
 
 import Foundation
-import ShellOut
+import RunsShellCommand
 
-enum ShellCommand {
+enum ShellCommand: AnyShellCommand {
     case createSwiftReleaseBuild
     case copyBuildArtefactToUserLocalBin(projectName: String, toolName: String)
-
+    
     var command: String {
-        switch self {
-        case .createSwiftReleaseBuild: return "swift"
-        case .copyBuildArtefactToUserLocalBin: return "cp"
+        get {
+            switch self {
+            case .createSwiftReleaseBuild: return "swift"
+            case .copyBuildArtefactToUserLocalBin: return "cp"
+            }
         }
+        set {}
     }
     
-    var arguments: [String] {
-        switch self {
-        case .createSwiftReleaseBuild: return ["build", "-c", "release"]
-        case .copyBuildArtefactToUserLocalBin(let projectName, let toolName): return ["-f", ".build/release/\(projectName)", "/usr/local/bin/\(toolName)"]
+    var defaultArguments: [String] {
+        get {
+            switch self {
+            case .createSwiftReleaseBuild: return ["build", "-c", "release"]
+            case .copyBuildArtefactToUserLocalBin(let projectName, let toolName): return ["-f", ".build/release/\(projectName)", "/usr/local/bin/\(toolName)"]
+                
+            }
         }
+        set {}
     }
     
-    var successMessage: String {
+    var successMessage: String? {
+        get {
             switch self {
             case .createSwiftReleaseBuild: return "Successfully created release artefacts."
             case .copyBuildArtefactToUserLocalBin(let projectName, let toolName): return "Successfully copied .build/release/\(projectName) to /usr/local/bin/\(toolName)). You can now run \(toolName) as a command from Terminal. :]"
             }
         }
-
-    func execute(onSuccess: ((String) -> Void)? = nil, onFailure: ((ShellOutError) -> Void)? = nil) {
-        do {
-            let output = try shellOut(to: command, arguments: arguments)
-            if let onSuccess = onSuccess {
-                onSuccess(output)
-            } else {
-                print(output)
-                print(successMessage)
-            }
-        } catch let error {
-            let error = error as! ShellOutError
-            if let onFailure = onFailure {
-                onFailure(error)
-            } else {
-                print(error.message) // Prints STDERR
-                print(error.output) // Prints STDOUT
-            }
-        }
+        set {}
     }
 }
